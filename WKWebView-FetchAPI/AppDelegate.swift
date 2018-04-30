64b0c5fd13acfaa74de13c05cf1e6612d4b56623
minikin <djminikin@gmail.com>
2018-04-30 08:39:32 +0200

    Initial commit

---
+++ WKWebView-FetchAPI/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  WKWebView-FetchAPI
+//
+//  Created by Sasha Prokhorenko on 30.04.18.
+//  Copyright © 2018 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
---
+++ WKWebView-FetchAPI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WKWebView-FetchAPI
+//
+//  Created by Sasha Prokhorenko on 30.04.18.
+//  Copyright © 2018 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
